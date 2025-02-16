8e23520a50858345876249524ea5291923b985ea
Daniela <nam991019@gmail.com>
2025-02-16 17:36:08 -0500

    agrego function

---
+++ script.js
@@ -0,0 +1,9 @@
+// se define el arreglo  de libros como objetos con titulo y año
+let libros = [
+    {titulo: "El Quijote",año: 1605, reservas: 50},
+    {titulo:"Cien años de soledad", año: 1949, reservas: 100},
+    {titulo: "1984", año: 1949, reservas:80},
+    {titulo: "La sombra del viento", año: 2001, reservas: 60}
+];
+
+//
