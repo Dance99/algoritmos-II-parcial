66acb494bc7aea997bc0534b341d16ebb0cce68d
Daniela <nam991019@gmail.com>
2025-02-16 17:39:34 -0500

    llamado del ciclo

--- script.js
+++ script.js
@@ -6,4 +6,25 @@ let libros = [
     {titulo: "La sombra del viento", año: 2001, reservas: 60}
 ];
 
-//
+//Algoritmos de seleccion para ordenar
+function OrdenaporTitulos(libros){
+    let n =libros.length;
+
+    for (let i = 0; i < n -1; i++) {
+        letminIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (libros[j].titulo < libros[minIndex].titulo){
+                minIndex = j; // con este encuentras el libro con el titulo mas corto
+            }
+        }
+        //se intercambia los libros
+        let temp = libros[i];
+        libros[i] = libros[minIndex];
+        libros[minIndex] = temp
+    }
+}
+
+//Llamo la funcion para ordenar los libros por titulo
+let m = OrdenaporTitulos(libros);
+ // se imprime el resultado 
+ console.log(libros);
