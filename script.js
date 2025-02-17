f5f4053190456bfc72c4e403acc00a237787be75
Daniela <nam991019@gmail.com>
2025-02-16 22:45:02 -0500

    algoritmo de sellecion

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // se define el arreglo  de libros como objetos con titulo y año
+// Ejemplo de estructura para un libro
 let libros = [
-    {titulo: "El Quijote",año: 1605, reservas: 50},
-    {titulo:"Cien años de soledad", año: 1949, reservas: 100},
-    {titulo: "1984", año: 1949, reservas:80},
-    {titulo: "La sombra del viento", año: 2001, reservas: 60}
+    {titulo: "El Principito", autor: "Antoine de Saint-Exupéry", año: 1943, reservas: 150},
+    {titulo: "1984", autor: "George Orwell", año: 1949, reservas: 200},
+    {titulo: "Cien Años de Soledad", autor: "Gabriel García Márquez", año: 1967, reservas: 300},
+    {titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes", año: 1605, reservas: 50},
+    {titulo: "La Odisea", autor: "Homero", año: -800, reservas: 120}
 ];
 
 //Algoritmos de seleccion para ordenar
 function OrdenaporTitulos(libros){
-    let n =libros.length;
-
-    for (let i = 0; i < n -1; i++) {
-        letminIndex = i;
-        for (let j = i + 1; j < n; j++) {
-            if (libros[j].titulo < libros[minIndex].titulo){
+    for (let i = 0; i < libros.length; i++) {
+        let minIndex = i;   //el primer elemento es mas pequeño
+        for (let j = i + 1; j < libros.length; j++) {
+            if (libros[j].año < libros[minIndex].año){
                 minIndex = j; // con este encuentras el libro con el titulo mas corto
             }
         }
@@ -22,9 +22,8 @@ function OrdenaporTitulos(libros){
         libros[i] = libros[minIndex];
         libros[minIndex] = temp
     }
+    return libros;
 }
 
 //Llamo la funcion para ordenar los libros por titulo
-let m = OrdenaporTitulos(libros);
- // se imprime el resultado 
- console.log(libros);
+console.log("\nOrden por reservas:", (OrdenaporTitulos(libros)));
