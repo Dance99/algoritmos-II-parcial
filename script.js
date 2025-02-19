5fe7f8800243bf7239cdd326365d775dc4164d2e
Daniela <nam991019@gmail.com>
2025-02-18 19:26:33 -0500

    algoritmo de insercion

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// se define el arreglo  de libros como objetos con titulo y año
+// se define el arreglo  de libros como objetos con titulo, año y reserva
 // Ejemplo de estructura para un libro
 let libros = [
     {titulo: "El Principito", autor: "Antoine de Saint-Exupéry", año: 1943, reservas: 150},
@@ -9,38 +9,44 @@ let libros = [
 ];
 
 //Algoritmos de seleccion para ordenar por año
-function OrdenaporTitulos(libros){
-    for (let i = 0; i < libros.length; i++) {
-        let minIndex = i;   //el primer elemento es mas pequeño
-        for (let j = i + 1; j < libros.length; j++) {
-            if (libros[j].año < libros[minIndex].año){
-                minIndex = j; // con este encuentras el libro con el titulo mas corto
-            }
+// function ordenarPorAño(libros) {
+//     let n = libros.length;
+//     for (let i = 0; i < n - 1; i++) {
+//         let minIndex = i;
+//         for (let j = i + 1; j < n; j++) {
+//             if (libros[j].año < libros[minIndex].año) {
+//                 minIndex = j;
+//             }
+//         }
+//         // Intercambio de elementos
+//         if (minIndex !== i) {
+//             [libros[i], libros[minIndex]] = [libros[minIndex], libros[i]];
+//         }
+//     }
+//     return libros;
+// }
+// //Llamo la funcion para ordenar los libros por titulo
+// console.time("OrdenamientoPorAño");
+// console.log("\nOrden por año:", (ordenarPorAño(libros)));
+// console.timeEnd("OrdenamientoPorAño");
+
+
+
+// Algoritmo de insercion por reserva
+function ordenarporReserva(libros) {
+    for (let i = 1; i < libros.length; i++) {
+        let currentBook = libros[i];
+        let j = i - 1;
+        while (j >= 0 && libros[j].reservas < currentBook.reservas) {
+            libros[j + 1] = libros[j]; //Mueve los elementos hacia la derecha
+            j--;
         }
-        //se intercambia los libros
-        let temp = libros[i];
-        libros[i] = libros[minIndex];
-        libros[minIndex] = temp
+libros[j + 1] = currentBook; //coloca el libro en posicion corecta
     }
     return libros;
 }
 
-//Llamo la funcion para ordenar los libros por titulo
-console.log("\nOrden por año:", (OrdenaporTitulos(libros)));
-
-// Algoritmo de insercion por reserva
-function ReservA(libro) {
-    for (let i = 1; i > libro.length; i++) {
-        let currentBook = libro[i];
-        let j = i - 1;
-        while (j <= 0 && libro[j].reservas < currentBook.reservas) {
-            libro[j + 1] = libro[j]; //Mueve los elementos hacia la derecha
-            j--;
-        }
-libro[j + 1] = currentBook; //coloca el libro en posicion corecta
-    }
-    return libro;
-}
-
 //realizo el llamdo 
-console.log ("\nOrden por reservas:", (ReservA(libros)));
+console.time("OrdenamientoPorReserva");
+console.log("\nOrden por Reserva:", ordenarporReserva(libros));
+console.timeEnd("OrdenamientoPorReserva");
