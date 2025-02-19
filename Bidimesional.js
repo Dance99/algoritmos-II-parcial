3307393ff523fb5d1621a87c49002ca83416dd24
Daniela <nam991019@gmail.com>
2025-02-18 23:15:08 -0500

    algoritmo de seleccion año

--- Bidimesional.js
+++ Bidimesional.js
@@ -8,21 +8,43 @@ let libros = [
 ];
 
 // Algoritmo de burbuja para ordenar por título
-function OrdenarPorTitulo(libros) {
-    for (let i = 0; i < libros.length - 1; i++) {
-        for (let j = 0; j < libros.length - i - 1; j++) {
-            if (libros[j][0].titulo.localeCompare(libros[j + 1][0].titulo) > 0) {
-                // Intercambia los libros
-                let temp = libros[j];
-                libros[j] = libros[j + 1];
-                libros[j + 1] = temp;
+// function OrdenarPorTitulo(libros) {
+//     for (let i = 0; i < libros.length - 1; i++) {
+//         for (let j = 0; j < libros.length - i - 1; j++) {
+//             if (libros[j][0].titulo.localeCompare(libros[j + 1][0].titulo) > 0) {
+//                 // Intercambia los libros
+//                 let temp = libros[j];
+//                 libros[j] = libros[j + 1];
+//                 libros[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return libros;
+// }
+
+// // Llamada a la función
+// console.time("OrdenamientoPorTitulo");
+// console.log("\nOrden por Título:", OrdenarPorTitulo(libros));
+// console.timeEnd("OrdenamientoPorTitulo");
+
+//Algoritmos de seleccion para ordenar por año
+function ordenarPorAño(libros) {
+    let n = libros.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (libros[j][0].año < libros[minIndex][0].año) {
+                minIndex = j;
             }
+        }
+        // Intercambio de elementos
+        if (minIndex !== i) {
+            [libros[i], libros[minIndex]] = [libros[minIndex], libros[i]];
         }
     }
     return libros;
 }
-
-// Llamada a la función
-console.time("OrdenamientoPorTitulo");
-console.log("\nOrden por Título:", OrdenarPorTitulo(libros));
-console.timeEnd("OrdenamientoPorTitulo");
+//Llamo la funcion para ordenar los libros por titulo
+console.time("OrdenamientoPorAño");
+console.log("\nOrden por año:", (ordenarPorAño(libros)));
+console.timeEnd("OrdenamientoPorAño");
